2a4d665b0e0f1a484d908288b1346cf163271378
Deckins <bennythai2013@hotmail.com>
2019-04-14 06:23:12 -0400

    nutrient stuff

--- brainsnapapi/index.js
+++ brainsnapapi/index.js
@@ -1,10 +1,15 @@
 // set "GOOGLE_APPLICATION_CREDENTIALS=./image_recog-894439ab6fff.json"
 const express = require('express');
 const uuidv4 = require('uuid/v4');
+const axios = require('axios');
 const formidableMiddleware = require('express-formidable');
 const app = express();
 const fs = require('fs');
 const path = require('path')
+const vision = require('@google-cloud/vision');
+const bodyParser = require('body-parser');  
+const url = require('url');  
+const querystring = require('querystring');  
 
 app.use(formidableMiddleware());
 app.use('/img', express.static(path.join(__dirname, 'img')))
@@ -16,12 +21,21 @@ app.get('/', (req, res) => {
 });
 
 async function quickstart(path) {
-  const vision = require('@google-cloud/vision');
   const client = new vision.ImageAnnotatorClient();
   const [result] = await client.labelDetection(path);
   console.log(result)
   return result;
 }
+
+app.get('/getfoodinfo', async (req, res) => {
+  let label = req.query.label;
+  if (label && label !== '') {
+    const apiurl = `https://api.edamam.com/api/food-database/parser?ingr=${encodeURIComponent(label)}&app_id=dacb9331&app_key=11135c95656912ac0149d761a2bc4071`
+    return res.send((await axios.get(apiurl)).data)
+  } else {
+    return res.status(400).send('No label was provided.');
+  }
+})
 
 app.post('/postpic', async (req, res) => {
   if (req.files && req.files.imageupload) {
--- src/components/views/Info.vue
+++ src/components/views/Info.vue
@@ -3,17 +3,21 @@
     <div class="pic-container mb-5">
       <img class="pic" :src="`/api/${$route.params.name}`" />
     </div>
-    <div class="food-card mb-2" v-for="(l, i) in descriptions" :key="i">
+    <div class="food-card mb-2" v-for="(p, i) in lst" :key="i">
       <div class="title-row">
-        <div class="title">{{ l }}</div>
+        <div class="title">{{ p.name }}</div>
         <div> {{true ? 'BRAIN FOOD' : ''}} </div>
       </div>
-      <div>
-        calories:
+      <div class="nutrient mx-auto pb-3">
+        <div class="nutrient-pair" v-for="([a,b],idx) in Object.entries(p.value.food.nutrients)"
+          :key="idx">
+          <div>{{a}}</div>
+          <div>{{b}}</div>
+        </div>
       </div>
-      <div>
-        calories:
-      </div>
+    </div>
+    <div v-if="lst.length === 0" class="food-card food-not-found mb-2">
+      This does not appear to be edible
     </div>
   </div>
 </template>
@@ -23,13 +27,43 @@ export default {
   name: 'Info',
   data () {
     return {
+      lst: []
     }
   },
   computed: {
     descriptions() {
-      return this.$route.params.value.labelAnnotations.map(l => l.description);
+      return this.$route.params.value &&
+        this.$route.params.value.labelAnnotations.map(l => l.description)
+        || [];
     }
-  }
+  },
+  methods: {
+    readInfo(l) {
+      return this.infoMap[l]
+    }
+  },
+  mounted() {
+    this.descriptions.forEach(d => {
+      this.axios.get(`/api/getfoodinfo?label=${encodeURIComponent(d)}`)
+        .then(x => {
+          let searched = x.data.hints.find(x => x.food.label.toUpperCase() === d.toUpperCase())
+          if (searched) {
+            this.lst.push({
+              name: d,
+              value: searched,
+            })
+          }
+        })
+    });
+  },
+  watch: {
+    infoMap: {
+      handler: function(val, oldVal) {
+        this.showMap();
+      },
+      deep: true
+    }
+  },
 }
 </script>
 
@@ -52,6 +86,9 @@ export default {
   .food-card {
     background-color: rgba(233, 161, 27, 0.8);
     border-radius: 5px;
+    &.food-not-found {
+      font-size: 25px;
+    }
     .title-row {
       margin: 0 10px;
       display: flex;
@@ -62,8 +99,14 @@ export default {
         text-decoration: underline;
       }
     }
-    .content {
-
+    .nutrient {
+      width: 100%;
+      max-width: 500px;
+      .nutrient-pair {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
     }
   }
 }
